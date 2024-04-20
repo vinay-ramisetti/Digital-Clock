7133701c6d82b474fac30804a069d974b633d09e
vinay-ramisetti <157636674+vinay-ramisetti@users.noreply.github.com>
2024-04-21 00:22:59 +0530

    Add files via upload

---
+++ Digital clock/script.js
@@ -0,0 +1,33 @@
+setInterval(()=>{
+    let hours=document.getElementById("hours");
+    let minutes=document.getElementById("minutes");
+    let seconds=document.getElementById("seconds");
+    let ampm=document.getElementById("ampm");
+    let h=new Date().getHours();
+    let m=new Date().getMinutes();
+    let s=new Date().getSeconds();
+    let am;
+    let hh=document.getElementById("hh");
+    let mm=document.getElementById("mm");
+    let ss=document.getElementById("ss");
+    let hr_dot=document.querySelector(".hr_dot");
+    let min_dot=document.querySelector(".min_dot");
+    let sec_dot=document.querySelector(".sec_dot");
+    if(h>=12)
+    am="PM";
+    else am="AM";
+    if(h>12) h=h-12;
+    if(h<10) h="0"+h;
+    if(m<10) m="0"+m;
+    if(s<10) s="0"+s;
+    hours.innerHTML=h+"<br><span>Hours</span></br>";
+    minutes.innerHTML=m+"<br><span>Minutes</span></br>";
+    seconds.innerHTML=s+"<br><span>Seconds</span></br>";
+    ampm.innerHTML=am;
+    hh.style.strokeDashoffset=440-(440*h)/12;
+    mm.style.strokeDashoffset=440-(440*m)/60;
+    ss.style.strokeDashoffset=440-(440*s)/60;
+    hr_dot.style.transform=`rotate(${h*30}deg)`;
+    min_dot.style.transform=`rotate(${m*6}deg)`;
+    sec_dot.style.transform=`rotate(${s*6}deg)`;
+},1000)
